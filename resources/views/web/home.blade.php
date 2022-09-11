b6e3695a1c90f756bab9771974daa2b13545a4e5
Partha Priyadarshi Das <partha.das36@gmail.com>
2022-09-11 16:55:10 +0530

    fix search button typo

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -181,8 +181,8 @@
             <input type="Number" id="full-name" name="full-name"
               class="bg-gray-100 bg-opacity-50 rounded  text-base text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
           </div>
-          <button type="submit" class="self-end inline-block px-6 py-2.5 text-sm font-medium text-white bg-indigo-600 border border-indigo-600 rounded active:text-indigo-500 focus:outline-none focus:ring" href="/download">
-            Download
+          <button type="submit" class="self-end inline-block px-6 py-2.5 text-sm font-medium text-white bg-indigo-600 border border-indigo-600 rounded active:text-indigo-500 focus:outline-none focus:ring" href="#">
+            Search
           </button>
         </form>
 
